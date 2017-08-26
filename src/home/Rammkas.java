47f41a0a3587376de7f7520110ac3fab2fed5c9b
Rammkas <rammi@list.ru>
2017-08-26 14:50:35 +0300

    testing git in intellij

---
+++ src/home/Rammkas.java
@@ -0,0 +1,12 @@
+package home;
+
+public class Rammkas {
+
+        public static void main(String[] args) {
+            int i = 10;
+            float f = 10;
+            System.out.println("Right");
+            System.out.println(i);
+            System.out.println(f);
+        }
+}
