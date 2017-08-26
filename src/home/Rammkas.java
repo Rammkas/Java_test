20e0b9e2efd8f413c81e68de2053533c6f5f0759
Rammkas <rammi@list.ru>
2017-08-26 15:09:39 +0300

    return double

--- src/home/Rammkas.java
+++ src/home/Rammkas.java
@@ -5,10 +5,10 @@ public class Rammkas {
         public static void main(String[] args) {
             int i = 10;
             float f = 10;
-            //double d = 10.0;
+            double d = 10.0;
             System.out.println("Right");
             System.out.println(i);
             System.out.println(f);
-            //System.out.println(d);
+            System.out.println(d);
         }
 }
